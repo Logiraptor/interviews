89c271708bac9e4cd120fc8c9f91c14f6611c81b
Patrick Oyarzun <patrickoyarzun@gmail.com>
2020-07-11 14:17:15 -0500

    Add cors configuration to the audio bucket.

--- modules/private_bucket/main.tf
+++ modules/private_bucket/main.tf
@@ -4,6 +4,13 @@ variable "name" {}
 resource "google_storage_bucket" "bucket" {
   name     = "${var.name}-${var.project}"
   location = "US"
+
+   cors {
+     max_age_seconds = 3600
+     method          = ["PUT"]
+     origin          = ["*"]
+     response_header = ["Content-Type"]
+   }
 }
 
 resource "google_storage_bucket_acl" "bucket-acl" {
